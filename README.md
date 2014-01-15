f31092cfa296d13d1e40206efbd7138a5d11e63e
Tom Booth <tombooth@gmail.com>
2014-01-15 21:15:35 +0000

    Initial project setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# paint
+
+A Clojure library that replicates painting on a substrate.
+
+Based on the Wet & Sticky method by Tunde Cockshott
+
+## License
+
+Copyright © 2014 Tom Booth
+
+Distributed under the Eclipse Public License
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject paint "0.1.0-SNAPSHOT"
+  :description "Library from paint emulation"
+  :url "https://github.com/tombooth/paint"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
